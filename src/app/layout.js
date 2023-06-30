5dd3a43d6adc01990b94413a46db26457d679dad
mehedi-imam241 <1805039@ugrad.cse.buet.ac.bd>
2023-06-30 17:07:08 +0600

    changed description for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const inter = Ubuntu({
 
 export const metadata = {
   title: "BUET CSE Fest 2023",
-  description: "BUET CSE Fest 2023",
+  description: "Welcome to BUET CSE Fest 2023! Join us for an exceptional celebration of innovation and technology. Engage in coding contests, explore emerging trends, and forge lasting connections. Showcasing talent and passion, let's make a profound impact together."
 };
 
 export default function RootLayout({ children }) {
